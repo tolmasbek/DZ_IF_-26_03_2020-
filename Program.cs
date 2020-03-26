f0b493721fb69b89b1da147603e29480b57b353d
tolmasbek <62470053+tolmasbek@users.noreply.github.com>
2020-03-26 22:54:01 +0500

    dz_c#_26_03_2020

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DZ_if_26_03_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
