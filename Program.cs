5e37ebb3ebb90425dd243b5626b380b142cb9db6
tolmasbek <62470053+tolmasbek@users.noreply.github.com>
2020-03-27 00:03:58 +0500

    comment

--- Program.cs
+++ Program.cs
@@ -11,6 +11,27 @@ namespace DZ_if_26_03_2020
                 Выучите основные конструкции и понятия, рассмотренные на уроке
             */
             Console.WriteLine("OK!");
+
+            /*
+                Задание 2
+                    Написать программу вычисления стоимости покупки с учетом скидки. Скидка в 3%
+                    предоставляется в том случае, если сумма покупки больше 500 сомони, в 5% - если
+                    сумма больше 1000 сомони
+            */
+
+            decimal sumPurchase;
+            decimal discount = 0;
+            Console.Write("sum_purchase = ");
+            sumPurchase = decimal.Parse(Console.Read());
+            if(sumPurchase >= 500)
+            {
+                discount = (sumPurchase * 3) / 100;
+            }
+            if(sumPurchase >= 1000)
+            {
+                discount = (sumPurchase * 5) / 100;
+            }
+            Console.WriteLine(discount);
             
             Console.ReadKey();
         }
